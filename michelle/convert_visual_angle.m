187af77a91ed9e649e397ab1cbf78cdde088ed7c
jiakunf <jiakun.fu.bcm@gmail.com>
2024-05-03 14:07:10 -0700

    update

--- michelle/convert_visual_angle.m
+++ michelle/convert_visual_angle.m
@@ -1,7 +1,7 @@
 function [pixel_per_degree, visual_angle] = convert_visual_angle(distance, width, npixel)
     % distance and width in centimeter
-    % npixel: number of pixels in width
-    % return pixels per degree of visual angle, and total visual angle in monitor width
+    % npixel: number of pixels in monitor width
+    % return number of pixels per degree of visual angle, and total visual angle in monitor width
 
         visual_angle = 2 * atan(width / (2 * distance));
         pixel_per_degree = npixel / visual_angle;
